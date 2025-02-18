b3bfb042957cb3ac1aae67261c82db61ac1885c1
rodrigocra1 <155087984+rodrigocra1@users.noreply.github.com>
2025-02-18 23:20:29 +0000

    Primeira interação

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fac_projects
+
+Repositório de armazenamento das atividades do curso de Ciências da Computação - Faculdade Estácio.
---
+++ Trunfo/trunfo1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+/* Essa versão pode ser considerada mais simples;
+ela imprime cada carta logo após o input do usuário. */
+
+void criar_carta(); // função de criar carta
+void print_carta(); // função de imprimir carta
+
+// Definindo variáveis
+char estado, codigo_carta[50], cidade[50];
+int populacao, pt_turisticos;
+float area, pib;
+int carta = 1;
+
+int main(void)
+{
+    do // repete a função de criar carta e imprimir carta assim que o usuário terminar seu input.
+    {
+        criar_carta();
+        printf("\n");
+        print_carta();
+        printf("\n");
+        carta++;
+    } while (carta == 2); // número de cartas totais definido em 2.
+
+    return 0;
+}
+
+void criar_carta(void)
+{
+    printf("Digite os dados para a carta %d: \n", carta);
+
+    // Pedindo input do usuário
+    printf("Código do estado: ");
+    scanf(" %c", &estado);
+
+    printf("Código da carta: ");
+    scanf("%3s", codigo_carta);
+
+    printf("Cidade: ");
+    scanf("%s", cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pt_turisticos);
+}
+
+void print_carta(void)
+{
+    printf("Carta: %d\n", carta); // imprime o número da carta
+
+    // imprime resultados dos inputs:
+
+    printf("Código do estado: %c\n", estado);
+    printf("Código da carta: %3s\n", codigo_carta);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Pontos turísticos: %d\n", pt_turisticos);
+}
